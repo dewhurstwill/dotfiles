a41a8e49662dadfac38f54b4af929c902bc100f0
Will Dewhurst <34936646+dewhurstwill@users.noreply.github.com>
2020-10-27 16:02:00 +0000

    Create 1-install-via-curl.sh

---
+++ setup/1-install-via-curl.sh
@@ -0,0 +1,3 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+curl -fsSL -o brew_packages_list.txt https://raw.githubusercontent.com/dewhurstwill/dotfiles/main/setup/brew_packages_list.txt
+while read line; do brew install "$line";done < brew_packages_list.txt
